a6de6bbafceed9e69ed38e6791266021dec36196
Jared Brannen <jaredtylerbrannen678@gmail.com>
2025-04-23 19:56:51 -0500

    Updated for Tag Helper and View Component

---
+++ TicketApplication/ViewComponents/TicketStatusViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace TicketApplication.ViewComponents
+{
+    public class TicketStatusViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string statusName)
+        {
+            return View("Default", statusName);
+        }
+    }
+}
